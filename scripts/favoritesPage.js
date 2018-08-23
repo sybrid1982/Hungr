02468688143941a7780b08ebecbf475cdf3c0e2c
jalen <jalenparham97@gmail.com>
2018-08-23 18:23:17 -0400

    favorites page styled and took out search bar

--- scripts/favoritesPage.js
+++ scripts/favoritesPage.js
@@ -3,33 +3,36 @@
 const favoritesPage = {
   template: `
   <div class="container">
-    <header class="results__header">
+    <header class="favorites__header">
       <nav class="results__header--nav">
         <a href="#!/search" class="logo">Hungr</a>
         <h1 class="fav--title">My Favorite Recipes</h1>
-        <button class="search--button">Search</button>
-        <button ng-click="$ctrl.location()" class="fav--button">Favorites</button>
+        <button ng-click="$ctrl.location()" class="search--button">Search</button>
       </nav>
     </header>
     <section class="favorites__container">
       <ul class="favorites__list">
           <li ng-repeat="recipe in $ctrl.favorites track by $index">
           <button class="delete--button" ng-click="$ctrl.deleteFavorites($index)">Delete</button>
-          <img src="{{ recipe.image }}">
-          <h1>{{ recipe.label }}</h1>
-          <p>{{ recipe.source }}</p>
+          <a href="{{ recipe.url }}" target="_blank"><img src="{{ recipe.image }}"></a>
+          <h1 class="recipe__label">{{ recipe.label }}</h1>
+          <p>Source: <span>{{ recipe.source }}<span></p>
           </li>
       </ul>
     </section>
   </div>
   `,
 
-  controller: ["FavoritesService", function(FavoritesService) {
+  controller: ["FavoritesService", "$location", function(FavoritesService, $location) {
     const vm = this;
     vm.favorites = FavoritesService.favorites;
     vm.deleteFavorites = (index) => {
       FavoritesService.deleteFavorites(index);
     }
+    vm.location = () => {
+      $location.url("/search");
+    }
+
   }]
 }
 
