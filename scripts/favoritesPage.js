7d1df01ff56dee12927df467917cf8801c7dda53
Ashley Renick <renickmail@gmail.com>
2018-08-23 14:51:46 -0400

    linked the images

--- scripts/favoritesPage.js
+++ scripts/favoritesPage.js
@@ -2,9 +2,11 @@
 
 const favoritesPage = {
   template: `
+  <search-bar></search-bar>
   <section class="recipe__container">
     <ul class="recipe__list">
         <li ng-repeat="recipe in $ctrl.favorites track by $index">
+        <a href="{{ recipe.url }}" target="_blank" ><img src="{{ recipe.image }}"></a>
           <h1>{{ recipe.label }}</h1>
           <p>{{ recipe.source }}</p>
           <button ng-click="$ctrl.deleteFavorites($index)">Delete</button>
