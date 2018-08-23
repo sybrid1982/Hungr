04d261917fe9b4dc5b9f4bf98059e21f0cb12ed3
jalen <jalenparham97@gmail.com>
2018-08-23 10:35:06 -0400

    added background to search page

--- scripts/recipeList.js
+++ scripts/recipeList.js
@@ -1,22 +1,7 @@
 "use strict";
 
 const recipeList = {
-<<<<<<< HEAD
-    template: `
-        <section class="recipe__container">
-            <ul class="recipe__list">
-                <li recipe="recipe" index="$index" ng-repeat="recipe in $ctrl.results track by $index">
-                   <h1>{{ recipe.label }}</h1>
-                   <p>{{ recipe.source }}</p>
-                   <button ng-click="$ctrl.addToFavorites(recipe)">Add To Favorites</button>
-                </li>
-            </ul>
-            <button ng-click='$ctrl.getMoreResults()'>More Results</button>
-        </section>
-    `,
-=======
     templateUrl: "recipeList.html",
->>>>>>> 03ccb972e9984858de0a6e5466727c75705560aa
 
     controller: ["RecipePull", "FavoritesService", function(RecipePull, FavoritesService) {
         const vm = this;
