ade95a7a708c2820c884654d63cee7ea06e13c95
jalen <jalenparham97@gmail.com>
2018-08-24 16:48:17 -0400

    results page restyled

--- scripts/favoritesPage.js
+++ scripts/favoritesPage.js
@@ -7,7 +7,7 @@ const favoritesPage = {
       <nav class="results__header--nav">
         <a href="#!/search" class="logo">Hungr</a>
         <h1 class="fav--title">My Favorite Recipes</h1>
-        <button ng-click="$ctrl.location()" class="search--button">Search</button>
+        <button ng-click="$ctrl.location()" class="search-button">Search</button>
       </nav>
     </header>
     <section class="favorites__container">
@@ -17,6 +17,10 @@ const favoritesPage = {
           <a href="{{ recipe.url }}" target="_blank"><img src="{{ recipe.image }}"></a>
           <h1 class="recipe__label">{{ recipe.label }}</h1>
           <p>Source: <span>{{ recipe.source }}<span></p>
+          <details>
+            <summary>Ingredients</summary>
+            <p ng-repeat="ingredient in recipe.ingredients">- {{ ingredient.text }}</p>
+          </details>
           </li>
       </ul>
     </section>
@@ -30,7 +34,7 @@ const favoritesPage = {
       FavoritesService.deleteFavorites(index);
     }
     vm.location = () => {
-      $location.url("/search");
+      $location.url("/results");
     }
 
   }]
